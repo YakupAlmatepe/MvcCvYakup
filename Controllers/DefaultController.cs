6d8fc697d49dde6b2e8ab63eef8cfc21cce86e86
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-04-17 07:40:05 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvYakup.Models;
+using MvcCvYakup.Models.Entity;
+
+namespace MvcCvYakup.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvYakupEntities db = new DbCvYakupEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+           
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler=db.TblDeneyimler.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimler()
+        {
+            var egitimler = db.TblEgitim.ToList();
+            return PartialView(egitimler);
+        }
+    }
+}
